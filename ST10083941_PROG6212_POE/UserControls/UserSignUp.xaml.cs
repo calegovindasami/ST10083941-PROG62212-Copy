9dc89f0ea62b3576d8c9185e7c5b4df7c8fa3de0
CG-PC <cgov0406@gmail.com>
2022-09-12 20:02:27 +0200

    Added sign up user control.

---
+++ ST10083941_PROG6212_POE/UserControls/UserSignUp.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10083941_PROG6212_POE.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UserSignUp.xaml
+    /// </summary>
+    public partial class UserSignUp : UserControl
+    {
+
+        //Properties to retrieve the data stored within the input fields for the user.
+        public string Username
+        {
+            get => txbUsername.Text;
+            set => txbUsername.Text = value;
+        }
+
+        public int NumberOfSemesterWeeks
+        {
+            get => Convert.ToInt32(nudNumberOfSemesterWeeks.Value);
+            set => nudNumberOfSemesterWeeks.Value = value;
+        }
+
+        public DateTime? SemesterStartDate
+        {
+            get => dpSemesterStartDate.SelectedDate;
+            set => dpSemesterStartDate.SelectedDate = value;
+        }
+
+        public UserSignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
